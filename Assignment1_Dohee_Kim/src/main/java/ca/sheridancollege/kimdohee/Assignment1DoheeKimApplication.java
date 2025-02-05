8604954b7e41c0c2daab37bfc8093d29d11fed46
doh6077 <kimdohee@sheridancollege.ca>
2025-02-05 11:53:32 -0500

    First Commit

---
+++ Assignment1_Dohee_Kim/src/main/java/ca/sheridancollege/kimdohee/Assignment1DoheeKimApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.kimdohee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1DoheeKimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment1DoheeKimApplication.class, args);
+	}
+
+}
