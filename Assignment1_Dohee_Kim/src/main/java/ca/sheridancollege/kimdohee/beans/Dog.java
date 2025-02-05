31b13e660364fd62655f9d87d02d3ba70e8b41a5
doh6077 <kimdohee@sheridancollege.ca>
2025-02-05 12:03:02 -0500

    Define attributes for Dog entity including id, name, List of Dogs

--- Assignment1_Dohee_Kim/src/main/java/ca/sheridancollege/kimdohee/beans/Dog.java
+++ Assignment1_Dohee_Kim/src/main/java/ca/sheridancollege/kimdohee/beans/Dog.java
@@ -1,5 +1,28 @@
 package ca.sheridancollege.kimdohee.beans;
 
-public class Dog {
+import java.math.BigDecimal;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Dog {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String firstName;
+	private String lastName; 
+	private List<Judge> judges; 
 }
