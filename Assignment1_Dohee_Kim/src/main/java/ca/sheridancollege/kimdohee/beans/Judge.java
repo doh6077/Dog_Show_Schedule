afa8bb17f47643bcc0be223b71c3c016a66d496c
doh6077 <kimdohee@sheridancollege.ca>
2025-02-05 12:01:58 -0500

    Define attributes for Judge entity including ID, firstName, lastName, and associated dogs

--- Assignment1_Dohee_Kim/src/main/java/ca/sheridancollege/kimdohee/beans/Judge.java
+++ Assignment1_Dohee_Kim/src/main/java/ca/sheridancollege/kimdohee/beans/Judge.java
@@ -1,5 +1,25 @@
 package ca.sheridancollege.kimdohee.beans;
 
-public class Judge {
+import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Judge {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String firstName;
+	private String lastName; 
+	private List<Dog> dogs; 
 }
